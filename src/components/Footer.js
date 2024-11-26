3ee5b8addbd5c3a348ff9c24b2083aaffd692da4
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-11-26 17:40:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import {Box, Typography} from '@mui/material';
 const Footer = _ => {
   return (
     <Box pb={4}>
-      <Typography variant='h6' align='center'>Developed by Dnyaneshwar Wagh</Typography>
+      <Typography variant='h6' align='center'>Developed by Aayush &amp; Team</Typography>
       <Typography variant='h6' align='center'>
-        <a href="https://github.com/dnyaneshwarwagh0072/Cinematic" target="_blank" rel="noreferrer">
+        <a href="https://github.com/aayusharyan/cinematic" target="_blank" rel="noreferrer">
           Github Source Code
         </a>
       </Typography>
