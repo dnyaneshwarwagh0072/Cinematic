107fdf879406899fba704d07c9c6073169f20a18
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-11-26 17:33:16 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const omdb = axios.create({
-    baseURL: "http://www.omdbapi.com/",
-    timeout: 15000,
-    params: {
-        apikey: "a996e9db"
-    }
-})
-export { omdb };
+  baseURL: "https://www.omdbapi.com/",
+  timeout: 15000,
+  params: {
+    apikey: "a996e9db"
+  }
+});
+
+export {omdb};
